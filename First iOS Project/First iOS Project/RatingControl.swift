363fef7113d65e518b29d17b4d43233a45f95e5c
Arpit Jasapara <lordtipra@gmail.com>
2017-09-09 23:16:21 -0700

    Added basic custom control functionality

--- First iOS Project/First iOS Project/RatingControl.swift
+++ First iOS Project/First iOS Project/RatingControl.swift
@@ -11,10 +11,21 @@ import UIKit
 class RatingControl: UIStackView {
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    private func setupButtons() {
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        addArrangedSubview(button)
     }
 
 }
