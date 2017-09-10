a8ea7aa06f62c03fc842b53ab8da5a054e236e85
Arpit Jasapara <lordtipra@gmail.com>
2017-09-10 00:17:55 -0700

    Connected ratings to ViewController and cleaned up UI

--- First iOS Project/First iOS Project/ViewController.swift
+++ First iOS Project/First iOS Project/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -53,9 +55,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
 
     // Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
     @IBAction func selectPhotoFromLibrary(_ sender: UITapGestureRecognizer) {
         nameTextField.resignFirstResponder()
         let imagePickerController = UIImagePickerController()
