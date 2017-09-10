0d60e1e1f14bfab38e4744158657de95afca570e
Arpit Jasapara <lordtipra@gmail.com>
2017-09-09 23:08:53 -0700

    Added UIStackView to implement Ratings

---
+++ First iOS Project/First iOS Project/RatingControl.swift
@@ -0,0 +1,20 @@
+//
+//  RatingControl.swift
+//  First iOS Project
+//
+//  Created by Arpit Jasapara on 9/9/17.
+//  Copyright © 2017 Arpit Jasapara. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
