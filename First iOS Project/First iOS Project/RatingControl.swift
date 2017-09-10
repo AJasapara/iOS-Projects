14da208781efa4502288260fb80d7e7b4265a415
Arpit Jasapara <lordtipra@gmail.com>
2017-09-09 23:30:43 -0700

    Added 5 buttons

--- First iOS Project/First iOS Project/RatingControl.swift
+++ First iOS Project/First iOS Project/RatingControl.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+    private var ratingButtons = [UIButton]()
+    var rating = 0
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0)
+    @IBInspectable var starCount: Int = 5
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupButtons()
@@ -20,13 +26,17 @@ class RatingControl: UIStackView {
     }
     
     private func setupButtons() {
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-        addArrangedSubview(button)
+        for _ in 0..<5 {
+            let button = UIButton()
+            
+            button.backgroundColor = UIColor.red
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            addArrangedSubview(button)
+            ratingButtons.append(button)
+        }
     }
     
     func ratingButtonTapped(button: UIButton){
